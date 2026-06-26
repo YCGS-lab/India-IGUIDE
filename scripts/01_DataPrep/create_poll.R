5812e18eac808d2ff474e8a0087230084a9747b3
Emily <emily.goddard@yale.edu>
2026-06-26 16:27:58 -0400

    [IGUIDE] Update poll

--- scripts/01_DataPrep/create_poll.R
+++ scripts/01_DataPrep/create_poll.R
@@ -45,11 +45,75 @@ xwalk <- read.xlsx(paste0(xwalk_path, "xwalk_full.xlsx"))
 # Covariates
 covars <- readRDS(paste0(dropbox, "covariates/covariates.rds"))
 #==============================================================================#
-# 2.0 Subset Data ####
+# 2. Clean Demographic Variables to Match Census ####
+#==============================================================================#
+#------------------------------------------------------------------------------#
+## 2.1. Gender ####
+#------------------------------------------------------------------------------#
+ccim$demo_gender <- ""
+ccim$demo_gender[ccim$gender=="Male"] <- "Male"
+ccim$demo_gender[ccim$gender=="Female"] <- "Female"
+table(ccim$gender, ccim$demo_gender)
+
+#------------------------------------------------------------------------------#
+## 2.2. Age Group ####
+#------------------------------------------------------------------------------#
+ccim$demo_age <- ""
+ccim$demo_age[ccim$age<=29] <- "18-29"
+ccim$demo_age[ccim$age>=30 & ccim$age<=44] <- "30-44"
+ccim$demo_age[ccim$age>=45 &  ccim$age<=59] <- "45-59"
+ccim$demo_age[ccim$age>=60] <- "60+"
+ccim$demo_age[ccim$age==100] <- "60+"
+ccim$demo_age[ccim$age=="Not asked"] <- ""
+table(ccim$age, ccim$demo_age)
+
+#------------------------------------------------------------------------------#
+## 2.3. Caste ####
+#------------------------------------------------------------------------------#
+ccim$incl_in_caste <- 0
+ccim$incl_in_caste[ccim$religion %in% c("Hindu","Sikh","Buddhist/Neo Buddhist")] <- 1
+
+ccim$demo_caste <- "Other Castes"
+ccim$demo_caste[ccim$incl_in_caste==1 & ccim$caste=="Refused"] <- ""
+ccim$demo_caste[ccim$incl_in_caste==1 & ccim$caste=="Scheduled Caste"] <- "Scheduled Castes/Tribes"
+ccim$demo_caste[ccim$incl_in_caste==1 & ccim$caste=="Scheduled Tribe"] <- "Scheduled Castes/Tribes"
+table(ccim$caste, ccim$demo_caste, useNA = 'always')
+
+#------------------------------------------------------------------------------#
+## 2.4. Urban-Rural ####
+#------------------------------------------------------------------------------#
+ccim$demo_urban <- ""
+ccim$demo_urban[ccim$urban=="Urban"] <- "Urban"
+ccim$demo_urban[ccim$urban=="Semi-urban"] <- "Urban"
+ccim$demo_urban[ccim$urban=="Rural"] <- "Rural"
+table(ccim$urban, ccim$demo_urban)
+
+#------------------------------------------------------------------------------#
+## 2.5. Reorder Variables in Dataset ####
+#------------------------------------------------------------------------------#
+ccim <- ccim %>% 
+  dplyr::select(caseid, state, district, demo_urban, demo_gender, 
+                demo_age, demo_caste, everything())
+
+#------------------------------------------------------------------------------#
+## 2.6. Limit Dataset to Only the Observations that Have Full Demographics ####
+#------------------------------------------------------------------------------#
+ccim <- ccim %>% 
+  dplyr::filter(demo_urban!="",
+                demo_gender!="",
+                demo_age!="",
+                demo_caste!="")
+
+#==============================================================================#
+# 3.0 Subset Data ####
 #==============================================================================#
 poll <- ccim %>%
-  dplyr::select(caseid, weight, wave, state, district, age, gender, caste, 
-                religion, urban_rural, starts_with("n7")) %>%
+  dplyr::select(caseid, weight, wave, state, district, demo_age, demo_gender, 
+                demo_caste, demo_urban, starts_with("n7")) %>%
+  dplyr::rename(age = demo_age,
+                gender = demo_gender,
+                caste = demo_caste,
+                urban = demo_urban) %>%
   dplyr::filter(wave=="tapp", 
                 !is.na(weight)) %>%
   dplyr::mutate(state_district = paste0(state, ", ", district)) %>%
@@ -64,7 +128,29 @@ poll <- poll[!poll$district %in% remove_states,]
 
 rm(ccim)
 #==============================================================================#
-# 3.0 Merge Covariates ####
+# 4.0 Recode Data ####
+#==============================================================================#
+cols <- colnames(poll)[grepl("^n7", colnames(poll))]
+for(i in 1:length(cols)){
+  col <- cols[i]
+  poll$recode <- case_when(poll[,col] %in% c("Moderately worried", "Very worried") ~ 1, 
+                           poll[,col] %in% c("Not at all worried", "Not very worried", 
+                                             "Refused", "Don't know") ~ 0, 
+                           .default = NA)
+  poll$recode_oppose <- case_when(poll[,col] %in% c("Not at all worried", "Not very worried") ~ 1, 
+                                  poll[,col] %in% c("Moderately worried", "Very worried", 
+                                                    "Refused", "Don't know") ~ 0, 
+                                  .default = NA)
+  
+  colnames(poll)[colnames(poll)=="recode"] <- paste0(col,"_recode")
+  colnames(poll)[colnames(poll)=="recode_oppose"] <- paste0(col,"_oppose_recode")
+}
+
+poll <- poll %>%
+  dplyr::select(caseid, weight, wave, state, district, state_district,
+                age, gender, caste, urban, ends_with("_recode"))
+#==============================================================================#
+# 4.0 Merge Covariates ####
 #==============================================================================#
 # Limit Covariate Data to Survey
 state_dist <- unique(poll$state_district)
@@ -90,7 +176,7 @@ countna <- function(x){sum(is.na(x))}
 nans <- lapply(poll_covars, countna)
 
 #==============================================================================#
-# 4.0 Write Data ####
+# 5.0 Write Data ####
 #==============================================================================#
 # Write data
 write_rds(poll, paste0(dropbox, "poll/poll.rds"))
